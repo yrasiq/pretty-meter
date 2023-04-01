b855d301a4dd5ba79b6821ecef4f58abf15bcaec
yrasiq <yrasiq@gmail.com>
2023-04-01 14:55:01 +0300

    init inferenceService

---
+++ inferenceService/app.py
@@ -0,0 +1,74 @@
+import sys
+sys.path.append('../')
+
+import torch
+import configparser
+from fastapi import FastAPI
+from utils import test_transform
+from pydantic import BaseModel
+from base64 import b64decode
+from PIL import Image
+from io import BytesIO
+
+
+class Data(BaseModel):
+    instances: list[str]
+
+
+class Result(BaseModel):
+    predictions: list[float]
+
+
+class Model:
+
+    def __init__(self, path: str, device: str) -> None:
+        self.model = torch.load(path).eval().to(device)
+        self.device = device
+
+    async def preprocess(self, data: Data) -> torch.Tensor:
+        return torch.stack(
+            [
+                test_transform(Image.open(BytesIO(b64decode(b64data))).convert('RGB'))
+                for b64data in data.instances
+            ]
+        ).to(self.device)
+
+    async def predict(self, data: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            return self.model(data)
+
+    async def postprocess(self, data: torch.Tensor) -> dict[str, list[float]]:
+        return {
+            'predictions': [
+                round(predict, 1)
+                for predict in
+                (data.flatten() * 10)
+                .clip(0, 10)
+                .cpu()
+                .numpy()
+                .tolist()
+            ]
+        }
+
+    async def result(self, data: Data) -> dict[str, list[float]]:
+        preprocess = await self.preprocess(data)
+        predict = await self.predict(preprocess)
+        postprocess = await self.postprocess(predict)
+        return postprocess
+
+
+config = configparser.ConfigParser()
+config.read('.cfg')
+print(config.sections())
+man_model = Model(config['DEFAULT']['man'], config['DEFAULT']['device'])
+woman_model = Model(config['DEFAULT']['woman'], config['DEFAULT']['device'])
+app = FastAPI()
+
+
+@app.post('/man/predict')
+async def man_predict(data: Data) -> Result:
+    return await man_model.result(data)
+
+@app.post('/woman/predict')
+async def woman_predict(data: Data) -> Result:
+    return await woman_model.result(data)
---
+++ inferenceService/test.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append('../')
+
+import pytest
+import json
+from httpx import AsyncClient
+from httpx import Response
+from app import app
+from base64 import b64encode
+
+
+base_url = 'http://test'
+
+
+async def get_predict(uri: str, img_path: str, instances_size: int, ac: AsyncClient) -> Response:
+    with open(img_path, 'rb') as f:
+        img = b64encode(f.read()).decode('utf-8')
+    data = json.dumps({'instances': [img] * instances_size})
+    return await ac.post(uri, data=data)
+
+def assert_predict(resp: Response, instances_size: int) -> None:
+    assert resp.status_code == 200
+    result = resp.json()
+    assert result.get('predictions')
+    assert len(result['predictions']) == instances_size
+    for predict in result['predictions']:
+        assert isinstance(predict, float)
+        assert predict >= 0.0
+        assert predict <= 10.0
+        assert len(str(predict)) == 3
+
+
+@pytest.mark.anyio
+async def test_predict() -> None:
+    instances_size = 2
+    async with AsyncClient(app=app, base_url=base_url) as ac:
+        resp_m = await get_predict('/man/predict', 'm_test_img', instances_size, ac)
+        resp_w = await get_predict('/woman/predict', 'w_test_img', instances_size, ac)
+    assert_predict(resp_m, instances_size)
+    assert_predict(resp_w, instances_size)
